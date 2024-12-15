60974f970101fe70be53c05bbf44776fd9e24875
EuanFitz <euan.fitzpatty@gmail.com>
2024-12-15 14:09:52 -0700

    Fixing header style

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,12 @@
 //header
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".navbar");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navBar.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
